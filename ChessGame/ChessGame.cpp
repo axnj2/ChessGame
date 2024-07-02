6d8129ac085b945023f3b45debf45b74d522f890
erentar <erentar2002@gmail.com>
2024-07-02 18:50:44 +0200

    Stylistic changes

--- ChessGame/ChessGame.cpp
+++ ChessGame/ChessGame.cpp
@@ -1,6 +1,5 @@
-#include "ChessGame.h"
+using namespace std;
 #include "Board.h"
-#include <iostream>
 #include <algorithm>
 #include "raylib.h"
 
@@ -8,25 +7,37 @@
 #define windowWidth  1300
 #define sideBuffer 10
 
-int main(void)
-{   
-    InitWindow(windowWidth, windowHeight, "ChessBoard");
-    SetTargetFPS(144); // avoids the game running at crazy 3000 FPS or more using 10% of CPU 
-    
+void drawWindow(Board* board){
+    ClearBackground(RAYWHITE);
+    board->drawBoard();
+    DrawFPS(windowWidth - 150, 25);
+}
 
+void processInput(Board* board){
+    if( IsMouseButtonPressed(0) ){
+        board->onMouseClick();
+    }
+}
+
+int main(void){
+    InitWindow(
+        windowWidth,
+        windowHeight,
+        "ChessBoard"
+    );
+    SetTargetFPS(144); // avoids the game running at crazy 3000 FPS or more using 10% of CPU 
 
     Board testBoard = Board(
         Color{ 238, 238, 210, 255 },
-        Color{ 118, 150, 86, 255 },
-        std::min(windowHeight, windowWidth) - 2 * sideBuffer, 
+        Color{ 118, 150,  86, 255 },
+        std::min(windowHeight, windowWidth) - 2 * sideBuffer, //square size
         Vector2{ sideBuffer ,sideBuffer }, // places the board on the left of the screen
         "rnbqkbnr/pppppppp/8/3p1p2/4B3/3p4/PPPPPPPp/RNBQKBNR w KQkq - 0 1"
     );
     // "rnbqkbnr/pppppppp/8/3p1p2/4B3/3p4/PPPPPPPp/RNBQKBNR w KQkq - 0 1"
     //"8/Q5pk/8/8/8/8/8/4K3 b KQkq - 0 1"
 
-    while (!WindowShouldClose())
-    {
+    while( !WindowShouldClose() ){
         BeginDrawing();
         drawWindow(&testBoard);
         EndDrawing();
@@ -36,17 +47,5 @@ int main(void)
     CloseWindow();
 
     return 0;
-}
-
-void drawWindow(Board *board) {
-    ClearBackground(RAYWHITE);
-    board->drawBoard();
-    DrawFPS(windowWidth - 150, 25);
-}
-
-void processInput(Board *board) {
-    if (IsMouseButtonPressed(0)) {
-        board->onMouseClick();
-    }
 }
 
